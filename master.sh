b8c2d1c7ae5c115e7d7b07fd2f3ba283690f4daa
Athos Palmer <athos@inbox.com>
2016-02-20 01:19:04 -0300

    Added more installation steps and prompts

--- master.sh
+++ master.sh
@@ -3,7 +3,10 @@
 YN_USER="yournode"
 YN_GROUP="yournode"
 YN_BASEPATH="/var/yournode"
-YN_MONGOPORT="35642"
+YN_MONGO_DEFAULT_PORT="35642"
+YN_MONGO_DEFAULT_ADMIN_PASS="Cj6U5CfSwyMFZ8"
+YN_MONGO_DEFAULT_USER="yournode"
+YN_MONGO_DEFAULT_PASS="Q#FH5ur4Y@2MBn"
 YN_DEFAULTS_REPOSITORY="git@bitbucket.org:yournode/yn-defaultpages.git"
 YN_AUTOMATION_REPOSITORY="git@bitbucket.org:yournode/yn-automation.git"
 
@@ -17,15 +20,40 @@ echo ""
 echo "          Master Server Installation"
 echo ""
 
+read -r -p "Do you want to install YourNode Master on this server? [y/N] " RESPONSE
+if [[ $RESPONSE =~ ^([yY])$ ]]
+then
+    echo ""
+else
+    exit
+fi
+
+read -p "Enter MongoDB port [$YN_MONGO_DEFAULT_PORT]: " YN_MONGO_PORT
+YN_MONGO_PORT=${YN_MONGO_PORT:-$YN_MONGO_DEFAULT_PORT}
+read -p "Enter MongoDB admin \"ynmdbAdmin\" password [$YN_MONGO_DEFAULT_ADMIN_PASS]: " YN_MONGO_ADMIN_PASS
+YN_MONGO_ADMIN_PASS=${YN_MONGO_ADMIN_PASS:-$YN_MONGO_DEFAULT_ADMIN_PASS}
+read -p "Enter MongoDB user [$YN_MONGO_DEFAULT_USER]: " YN_MONGO_USER
+YN_MONGO_USER=${YN_MONGO_USER:-$YN_MONGO_DEFAULT_USER}
+read -p "Enter MongoDB password [$YN_MONGO_DEFAULT_PASS]: " YN_MONGO_PASS
+YN_MONGO_PASS=${YN_MONGO_PASS:-$YN_MONGO_DEFAULT_PASS}
+
 echo "Installing dependencies..."
-echo "Installing Extra Packages for Enterprise Linux (EPEL) repositories."
-yum --y install epel-release
+echo "Installing Extra Packages for Enterprise Linux (EPEL) repositories..."
+yum -y install epel-release
 echo "Installing Node.js and NPM..."
-yum --y install nodejs npm
+yum -y install nodejs npm
 echo "Installing Git..."
-yum --y install git
+yum -y install git
+
+echo "Installing MongoDB repositories..."
+printf "[mongodb]\n" > /etc/yum.repos.d/mongodb.repo
+printf "name=MongoDB Repository\n" >> /etc/yum.repos.d/mongodb.repo
+printf "baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/\n" >> /etc/yum.repos.d/mongodb.repo
+printf "gpgcheck=0\n" >> /etc/yum.repos.d/mongodb.repo
+printf "enabled=1" >> /etc/yum.repos.d/mongodb.repo
+yum -y update
 echo "Installing MongoDB..."
-yum --y install mongodb-org mongodb-org-server
+yum -y install mongodb-org mongodb-org-server
 
 echo "Creating folder structure..."
 mkdir -p "$YN_BASEPATH"
@@ -38,7 +66,7 @@ mkdir -p "$YN_BASEPATH/lib"
 mkdir -p "$YN_BASEPATH/bin"
 
 echo "Creating MongoDB configuration file..."
-printf "port = $YN_MONGOPORT\n" > "$YN_BASEPATH/mongodb.conf"
+printf "port = $YN_MONGO_PORT\n" > "$YN_BASEPATH/mongodb.conf"
 printf "auth = true\n" >> "$YN_BASEPATH/mongodb.conf"
 printf "dbpath = \"$YN_BASEPATH/db\"\n" >> "$YN_BASEPATH/mongodb.conf"
 printf "logpath = \"$YN_BASEPATH/logs/mongodb.log\"\n" >> "$YN_BASEPATH/mongodb.conf"
@@ -60,6 +88,11 @@ systemctl enable yn-mongod
 echo "Starting yn-mongod..."
 systemctl start yn-mongod
 
+echo "Creating MongoDB admin user..."
+mongo 127.0.0.1:$YN_MONGO_PORT/admin --eval "db.createUser({user: \"ynmdbAdmin\", pwd: \"$YN_MONGO_ADMIN_PASS\", roles: [{role: \"userAdminAnyDatabase\", db: \"admin\"}]})"
+echo "Creating MongoDB user for YNDATA database..."
+mongo 127.0.0.1:$YN_MONGO_PORT/YNDATA -u ynmdbAdmin -p $YN_MONGO_ADMIN_PASS --authenticationDatabase admin --eval "db.createUser({user: \"$YN_MONGO_USER\", pwd: \"$YN_MONGO_PASS\", roles: [{role: \"readWrite\", db: \"YNDATA\"}]})"
+
 echo "Cloning YourNode Default Pages..."
 git clone "$YN_DEFAULTS_REPOSITORY" "$YN_BASEPATH/lib/defaultpages"
 
@@ -72,6 +105,11 @@ echo "Enabling nginx on system start"
 systemctl enable nginx
 echo "Starting nginx..."
 systemctl start nginx
+
+echo "Creating group $YN_GROUP..."
+groupadd "$YN_GROUP"
+echo "Creating user $YN_USER..."
+useradd -g $YN_GROUP $YN_USER
 
 echo "Cloning YourNode Automation Scripts..."
 git clone "$YN_AUTOMATION_REPOSITORY" "$YN_BASEPATH/lib/automation"
--- shard.sh
+++ shard.sh
@@ -5,6 +5,7 @@ YN_GROUP="yournode"
 YN_BASEPATH="/var/yournode"
 YN_APPLICATIONPATH="/home/yournode"
 YN_AUTOMATION_REPOSITORY="git@bitbucket.org:yournode/yn-automation.git"
+YN_HELLOWORLD_REPOSITORY="git@bitbucket.org:yournode/yn-helloworld.git"
 
 echo "__     __              _   _           _"
 echo "\ \   / /             | \ | |         | |"
@@ -16,13 +17,21 @@ echo ""
 echo "          Shard Server Installation"
 echo ""
 
+read -r -p "Do you want to install YourNode Shard on this server? [y/N] " RESPONSE
+if [[ $RESPONSE =~ ^([yY])$ ]]
+then
+    echo ""
+else
+    exit
+fi
+
 echo "Installing dependencies..."
-echo "Installing Extra Packages for Enterprise Linux (EPEL) repositories."
-yum --y install epel-release
+echo "Installing Extra Packages for Enterprise Linux (EPEL) repositories..."
+yum -y install epel-release
 echo "Installing Node.js and NPM..."
-yum --y install nodejs npm
+yum -y install nodejs npm
 echo "Installing Git..."
-yum --y install git
+yum -y install git
 
 echo "Creating folder structure..."
 mkdir -p "$YN_BASEPATH"
@@ -34,7 +43,7 @@ mkdir -p "$YN_BASEPATH/bin"
 echo "Creating group $YN_GROUP..."
 groupadd "$YN_GROUP"
 echo "Creating user $YN_USER..."
-useradd -d "$YN_APPLICATIONPATH" -g $YN_GROUP -s /sbin/nologin $YN_USER
+useradd -d "$YN_APPLICATIONPATH" -g $YN_GROUP $YN_USER
 
 echo "Clone YourNode Automation Scripts..."
 git clone "$YN_AUTOMATION_REPOSITORY" "$YN_BASEPATH/lib/automation"
@@ -43,6 +52,9 @@ echo "Installing YourNode Shard Automation..."
 ln -s "$YN_BASEPATH/lib/automation/shard/yn-shard.js" "$YN_BASEPATH/bin/yn-shard"
 ln -s "$YN_BASEPATH/lib/automation/shard/yournode-shard.conf" "$YN_BASEPATH/bin/yournode-shard.conf"
 export PATH=$PATH:$YN_BASEPATH/bin
+
+echo "Clone YourNode Hello Word App..."
+git clone "$YN_HELLOWORLD_REPOSITORY" "$YN_BASEPATH/lib/helloworld"
 
 echo "Changing folder and files owners..."
 chown -R "$YN_USER:$YN_GROUP" "$YN_BASEPATH"
